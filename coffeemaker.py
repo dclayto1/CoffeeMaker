9774378accf1784a0d6ce3ad080c6a4c14fe0b9b
Dylan Clayton <dclayto1@umbc.edu>
2016-02-05 18:38:08 -0500

    quickly made html template to start with, started working on the flask/python functions to do the coffee stuff

--- coffeemaker.py
+++ coffeemaker.py
@@ -1,6 +1,56 @@
-from flask import Flask
+import RPi.GPIO as GPIO
+import signal
+import sys
+
+#'Globals'
+PIN = 5 #pin number, not "GPIO5", it's actually "GPIO3" but "Pin No.5"
+off = "OFF"
+on = "ON"
+status = ""
+
+
+from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return render_template("coffeemaker.html", status)
 
 
 
+
+
+##################################
+# Functions for webserver to use #
+##################################
+
+
+#Making sure that the coffee pot is turned off if
+# server process ends
+def endProcess(signum, frame):
+    status = off
+    GPIO.output(PIN, False)
+    GPIO.cleanup()
+    sys.exit(0)
+signal.signal(signal.SIGINT, endProcess)
+
+
+def toggle():
+    if(status == off):
+        status = on
+        GPIO.output(PIN, False)
+    else:
+        status = off
+        GPIO.output(PIN, True)
+
+
+
+#Runs the Flask application
+if __name__ == "__main__":
+    status = off
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(PIN, GPIO.OUT)
+    GPIO.output(PIN, True)
+
+    app.run()
